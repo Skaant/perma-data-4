59140f37a7c3d1fc10f226044235249aba8a999c
imrok le bleu <romaric.ruga@gmail.com>
2019-04-05 17:27:51 +0200

    getByIdWithNames on normal search

--- functions/apps/api/root/_lowers/plants/_lowers/search/_methods/get/get.js
+++ functions/apps/api/root/_lowers/plants/_lowers/search/_methods/get/get.js
@@ -1,38 +1,38 @@
-const getById = require('../../../../../../../../provisioners/plants/getById/getById')
+const getByIdWithNames = require('../../../../../../../../provisioners/plants/getByIdWithNames/getByIdWithNames')
 const getByName = require('../../../../../../../../provisioners/plants/getByName/getByName')
 
 module.exports = req =>
   new Promise((resolve, reject) => {
     const { key, lang } = req.query
-    getById(key)
-      .then(results => {
-        if (!results.plants) {
-          reject({
-            message: 'wrong ids results format',  
-            results
-          })
-        } else if (results.plants.length > 0) {
-          resolve(results)
-        } else {
-          if (!lang) {
-            reject({
-              message: 'no lang specified'
-            })
-          } else {
-            getByName(key, lang)
-              .then(results => {
-                if (!results.plants) {
-                  reject({
-                    message: 'wrong names results format',  
-                    results
-                  })
-                } else {
-                  resolve(results)
-                }
-              })
-            .catch(err => reject(err))
-          }
-        }
+    if (!lang) {
+      reject({
+        message: 'no lang specified'
       })
+    } else {
+      getByIdWithNames(key, lang)
+        .then(results => {
+          if (!results.plants) {
+            reject({
+              message: 'wrong ids results format',  
+              results
+            })
+          } else if (results.plants.length > 0) {
+            resolve(results)
+          } else {
+              getByName(key, lang)
+                .then(results => {
+                  if (!results.plants) {
+                    reject({
+                      message: 'wrong names results format',  
+                      results
+                    })
+                  } else {
+                    resolve(results)
+                  }
+                })
+              .catch(err => reject(err))
+          }
+        })
       .catch(err => reject(err))
+    }
   })
