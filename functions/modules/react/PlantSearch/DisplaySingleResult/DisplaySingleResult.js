9afda6b93ee8237475f0be360a90393a468ebbbf
imrok le bleu <romaric.ruga@gmail.com>
2019-04-06 21:46:35 +0200

    fix single result button display

--- functions/modules/react/PlantSearch/DisplaySingleResult/DisplaySingleResult.js
+++ functions/modules/react/PlantSearch/DisplaySingleResult/DisplaySingleResult.js
@@ -9,24 +9,22 @@ export default ({
     <div className='row'>
       <p className='col-12 px-4 pt-2'>
         { translations.title }</p></div>
-    <div className='row'>
-      <div className='col-12 btn-group px-0 d-flex'>
-        <button type='button' className='btn btn-warning flex-grow-1'
-            onClick={ () => selectPlant(plant._id) }>
-          { plant._id }</button>
-        {
-          step === 1 && (
-            <button type='button' className='btn btn-info'
-                title={ translations.digDeeper }
-                onClick={ () => digDeeper() }>
-              ➰</button>
-          )
-        }
-        <button type='button' className='btn btn-danger'
-            title={ translations.dismiss }
-            onClick={ () => dismiss() }>
-          ️✖️</button>
-      </div>
+    <div className='col-12 btn-group px-0 d-flex'>
+      <button type='button' className='btn btn-warning flex-grow-1'
+          onClick={ () => selectPlant(plant._id) }>
+        { plant._id }</button>
+      {
+        step === 1 && (
+          <button type='button' className='btn btn-info'
+              title={ translations.digDeeper }
+              onClick={ () => digDeeper() }>
+            ➰</button>
+        )
+      }
+      <button type='button' className='btn btn-danger'
+          title={ translations.dismiss }
+          onClick={ () => dismiss() }>
+        ️✖️</button>
     </div>
   </div>
 )
