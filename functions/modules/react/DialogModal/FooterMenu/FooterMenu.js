aedc9754933f0b4a232ffeda79199705a67715d2
imrok le bleu <romaric.ruga@gmail.com>
2019-05-01 14:37:52 +0200

    display optional menu

--- functions/modules/react/DialogModal/FooterMenu/FooterMenu.js
+++ functions/modules/react/DialogModal/FooterMenu/FooterMenu.js
@@ -5,16 +5,39 @@ export default ({
   back, next,
   translations
 }) => (
-  <div className='modal-footer container pl-0'>
-    <div className='row w-100 pr-2 pr-3'>
-      <button type='button' className='btn btn-secondary col-12 col-sm-5 col-md-3 my-1'
-          onClick={ () => back() }>
-        { translations.back }
-      </button>
-      <button type='button' className='btn btn-info col-12 col-sm-5 col-md-3 offset-sm-2 offset-md-6 my-1'
-          onClick={ () => next() }>
-        { translations.next }
-      </button>
+  <React.Fragment>
+    {
+      scene.menu && (
+        <div className='modal-footer container pl-0'>
+          <div className='row w-100 pr-2 pr-3 d-flex justify-content-center'>
+            {
+              scene.menu.map(item => (
+                <button key={ item } type='button'
+                    className='btn btn-warning col-12 col-sm-6 col-lg-3 mx-2 my-1'
+                    onClick={ () => console.log(item) }>
+                  { item }
+                </button>
+              ))
+            }
+          </div>
+        </div>
+      )
+    }
+    <div className='modal-footer container pl-0'>
+      <div className='row w-100 pr-2 pr-3'>
+        <button type='button' className='btn btn-secondary col-12 col-sm-5 col-md-3 my-1'
+            onClick={ () => back() }>
+          { translations.back }
+        </button>
+        {
+          !scene.disabledNext && (
+            <button type='button' className='btn btn-info col-12 col-sm-5 col-md-3 offset-sm-2 offset-md-6 my-1'
+                onClick={ () => next() }>
+              { translations.next }
+            </button>
+          )
+        }
+      </div>
     </div>
-  </div>
+  </React.Fragment>
 )
