aef9e0fa81fb17b0254ed0305c81bd1832693074
imrok le bleu <romaric.ruga@gmail.com>
2019-05-27 06:49:16 +0200

    dialog font

--- functions/modules/react/DialogModal/_staticStyle/_staticStyle.js
+++ functions/modules/react/DialogModal/_staticStyle/_staticStyle.js
@@ -1,13 +1,27 @@
 export default `
+  @font-face {
+    font-family: dayRoman;
+    src: url('/font--day_roman/DAYROM__.ttf')
+  }
+
+  .content-display {
+    font-size: 1.2rem;
+  }
+
   .content-display p,
   .content-display h1,
   .content-display h3,
   .content-display h5 {
+    font-family: dayRoman;
     width: 100%;
     max-width: 600px;
     text-align: center;
     margin-left: auto;
     margin-right: auto;
+  }
+
+  .content-display p {
+    margin-top: 0.5rem;
   }
 
   .content-display h1 {
