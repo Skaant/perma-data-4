59d40e89232b82c3902dd7a659bc51ad64e618fc
imrok le bleu <romaric.ruga@gmail.com>
2019-06-03 13:26:08 +0200

    keep extracts in kolo-seed shell

--- functions/apps/api/root/_lowers/dialog/_methods/post/_methods/introduction/introduction.js
+++ functions/apps/api/root/_lowers/dialog/_methods/post/_methods/introduction/introduction.js
@@ -1,4 +1,4 @@
-const getIntroductionUser = require('./getIntroductionUser/getIntroductionUser')
+const getUpdatedUser = require('./getUpdatedUser/getUpdatedUser')
 const userRelated = require('../../../../../../../../_aggregations/userRelated/userRelated')
 
 module.exports = ({ uid, lang }) => 
@@ -7,36 +7,37 @@ module.exports = ({ uid, lang }) =>
       if (err) {
         reject(err)
       }
-      const baseUser = getIntroductionUser()
       const dbUsers = client.db('prod').collection('users')
-      dbUsers
-        .updateOne({ _id: uid },
-          {
-            $set: baseUser
-          })
-          .then(() => 
-            dbUsers
-              .aggregate(userRelated(uid, lang, [
-                'dialogs', 'doms', 'events']))            
-              .toArray((err, user) => {
-                if (err) {
-                  reject(err)
-                } else if (!user[0]) {
-                  reject({
-                    status: 404,
-                    title: 'user data aggregation error',
-                    message: 'no user data at the end of the pipeline'
-                  })
-                }
-                const { home } = baseUser
-                const { dialogs, doms, events } = user[0]
-                resolve({
-                  dialogs,
-                  doms,
-                  events,
-                  home
-                })
-              }))
+      getUpdatedUser(dbUsers, uid, lang)
+        .then(updatedUser =>
+          dbUsers
+            .updateOne({ _id: uid },
+              {
+                $set: updatedUser
+              })
+              .then(() => 
+                dbUsers
+                  .aggregate(userRelated(uid, lang, [
+                    'dialogs', 'doms', 'events']))            
+                  .toArray((err, user) => {
+                    if (err) {
+                      reject(err)
+                    } else if (!user[0]) {
+                      reject({
+                        status: 404,
+                        title: 'user data aggregation error',
+                        message: 'no user data at the end of the pipeline'
+                      })
+                    }
+                    const { home } = updatedUser
+                    const { dialogs, doms, events } = user[0]
+                    resolve({
+                      dialogs,
+                      doms,
+                      events,
+                      home
+                    })
+                  })))
           .catch(err => reject(err))
         })
   })
